f8651563f26972492a59ccf3c419541a26da8f5e
Gerard Falco <gerardfp@users.noreply.github.com>
2021-02-17 16:57:32 +0100

    Update Tetris.java

--- src/com/company/Tetris.java
+++ src/com/company/Tetris.java
@@ -6,29 +6,31 @@ public class Tetris {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int f = scanner.nextInt();
-        int c = scanner.nextInt();
+        int filas = scanner.nextInt();
+        int columnas = scanner.nextInt();
 
-        int[][] t = new int[f][c];
+        int[][] tablero = new int[filas][columnas];
 
-        for (int i = 0; i < f; i++) {
-            for (int j = 0; j < c; j++) {
-                t[i][j] = scanner.nextInt();
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                tablero[i][j] = scanner.nextInt();
             }
         }
 
-        for (int i = 0; i < f; i++) {
-            boolean completa = true;
-            for (int j = 0; j < c; j++) {
-                if (t[i][j] == 0) {
-                    completa = false;
+
+        for (int i = 0; i < filas; i++) {
+
+            boolean hayAlgunCero = false;
+            for (int j = 0; j < columnas; j++) {
+                if(tablero[i][j] == 0){
+                    hayAlgunCero = true;
                     break;
                 }
             }
 
-            if(!completa){
-                for (int j = 0; j < c; j++) {
-                    System.out.print(t[i][j] + " ");
+            if(hayAlgunCero) {
+                for (int j = 0; j < columnas; j++) {
+                    System.out.print(tablero[i][j] + " ");
                 }
                 System.out.println();
             }
